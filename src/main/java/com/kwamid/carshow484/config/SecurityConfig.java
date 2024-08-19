51ada0ca401a1821f35cf18a00dc58b45ec6de89
Kdzahini <BEEFFICTOGO@GMAIL.COM>
2024-08-19 17:46:36 -0400

    second commit

--- src/main/java/com/kwamid/carshow484/config/SecurityConfig.java
+++ src/main/java/com/kwamid/carshow484/config/SecurityConfig.java
@@ -1,10 +1,19 @@
 package com.kwamid.carshow484.config;
 
+import ch.qos.logback.core.CoreConstants;
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.sun.tools.javac.Main;
+import org.springframework.boot.origin.SystemEnvironmentOrigin;
 import org.springframework.context.annotation.Bean;
 
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throw Exception{
-        http
+
     }
+
+    public static void main(String[] args) {
+        System.out.println("rewttetter");
+    }
+
 }
